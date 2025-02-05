00e0dab5af88bd2e35f4624bfd990c1db055a14c
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2025-02-05 12:33:22 -0800

    Created HTML templates for most basic pages

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/proposals")
+def proposals():
+    return render_template("proposals.html")
